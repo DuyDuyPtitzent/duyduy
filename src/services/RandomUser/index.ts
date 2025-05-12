d41394a79265b3a0e324b2f95b965887b3d9cfad
DuyDuyPtitzent <Duydvdhtb172005@gmail.com>
2025-05-12 15:16:50 +0700

    update2

--- src/services/RandomUser/index.ts
+++ src/services/RandomUser/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'; //thư viện axios dùng để gọi api
 
 export const getData = async () => {
-	const res = await axios.get('https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole');
+	const res = await axios.get('https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole');// gọi api randomuser.me
 	return res;
 };
